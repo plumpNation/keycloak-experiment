7f4258fc2e29f9ecdd42b6037f345095ef2e64ca
Gavin King <gavin.king@cgi.com>
2023-10-16 22:12:23 +0200

    Add keycloak configuration

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  KeycloakConnectModule,
+  PolicyEnforcementMode,
+  TokenValidation,
+} from 'nest-keycloak-connect';
 
 async function bootstrap() {
+  KeycloakConnectModule.register({
+    authServerUrl: 'http://localhost:8080', // might be http://localhost:8080/auth for older keycloak versions
+    realm: 'master',
+    clientId: 'my-nestjs-app',
+    secret: 'secret',
+    policyEnforcement: PolicyEnforcementMode.PERMISSIVE, // optional
+    tokenValidation: TokenValidation.ONLINE, // optional
+  });
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
